e721aa6ca0448de04b559a42a4a8f0f10c4e4b70
Emre DEMİRÖZ <emredemiroz173@gmail.com>
2026-04-14 21:36:39 +0300

    aa

--- otopark/otopark.ino
+++ otopark/otopark.ino
@@ -22,40 +22,46 @@ const int IR_PARK[6] = {13, 14, 25, 26, 27, 34}; // P1 - P6
 // --- STANDART SERVO (SG90) AYARLARI ---
 #define SERVO_MIN_US       500
 #define SERVO_MAX_US       2400
-#define SERVO_OPEN_ANGLE   120  // Açık bariyer açısı
-#define SERVO_CLOSE_ANGLE  0    // Kapalı bariyer açısı
-#define SERVO_HOLD_MS      3500 // Araç geçişi için bariyerin açık kalma süresi
+#define SERVO_OPEN_ANGLE   45
+#define SERVO_CLOSE_ANGLE  179
+#define SERVO_HOLD_MS      1000
+
+// --- SENSÖR FİLTRE AYARLARI ---
+#define GIRIS_DEBOUNCE_MS  150
+#define CIKIS_DEBOUNCE_MS  150
 
 FirebaseData fbdoRead, fbdoWrite;
 FirebaseAuth auth;
 FirebaseConfig config;
+
 Servo girisServosu;
 Servo cikisServosu;
 
 String docPath = "artifacts/" + String(APP_ID) + "/public/data/parking/status";
 
-// --- GECİKME (DEBOUNCE) ZAMANLAYICILARI ---
-unsigned long entryDetectTime = 0;
-unsigned long exitDetectTime = 0;
+// Her park alanı için ayrı sayaçlar
+unsigned long slotDetectTime[6] = {0, 0, 0, 0, 0, 0};
+unsigned long slotClearTime[6]  = {0, 0, 0, 0, 0, 0};
 
-// Her park alanı için ayrı ayrı sayaçlar
-unsigned long slotDetectTime[6] = {0,0,0,0,0,0}; // Araç ne zamandır alanda?
-unsigned long slotClearTime[6] = {0,0,0,0,0,0};  // Alan ne zamandır boş?
-
-// Son Olarak Firebase'e Onaylanıp Gönderilmiş Durumlar
+// Firebase'e en son gönderilmiş durumlar
 bool lastSlotStates[6] = {false, false, false, false, false, false};
-bool carPaid = false; 
+bool carPaid = false;
 
-// Bariyer Durumları
+// Bariyer durumları
 unsigned long girisOpenTime = 0;
 bool girisOpen = false;
+
 unsigned long cikisOpenTime = 0;
 bool cikisOpen = false;
 
+// Giriş / çıkış sensör filtre değişkenleri
+unsigned long girisDetectStart = 0;
+unsigned long cikisDetectStart = 0;
+
 int getOccupiedCount() {
   int count = 0;
-  for(int i=0; i<6; i++) {
-    if(lastSlotStates[i]) count++;
+  for (int i = 0; i < 6; i++) {
+    if (lastSlotStates[i]) count++;
   }
   return count;
 }
@@ -65,33 +71,38 @@ void updateFirestoreBool(String key, bool value) {
   Firebase.Firestore.patchDocument(&fbdoWrite, FIREBASE_PROJECT_ID, "", docPath.c_str(), json.c_str(), key.c_str());
 }
 
-// --- STANDART SERVO AÇMA FONKSİYONU ---
-void openGate(Servo &servo, int pin, bool &isOpenFlag, unsigned long &timer) {
-  servo.write(SERVO_OPEN_ANGLE); // Bariyeri havaya kaldır
-  
+void openGate(Servo &servo, bool &isOpenFlag, unsigned long &timer) {
+  servo.write(SERVO_OPEN_ANGLE);
   isOpenFlag = true;
   timer = millis();
 }
 
-// --- STANDART SERVO KAPATMA FONKSİYONU ---
-void closeGateIfTimeout(Servo &servo, bool &isOpenFlag, unsigned long timer, String firebaseUrl) {
+void closeGateIfTimeout(Servo &servo, bool &isOpenFlag, unsigned long timer, String firebaseKey) {
   if (isOpenFlag && (millis() - timer > SERVO_HOLD_MS)) {
     servo.write(SERVO_CLOSE_ANGLE);
-    // DÜZELTME: detach() KALDIRILDI! Motor gücü kesilmeyecek, kapıyı taş gibi kilitli tutacak.
     isOpenFlag = false;
-    updateFirestoreBool(firebaseUrl, false);
-    Serial.println("Bariyer kapatildi.");
+    updateFirestoreBool(firebaseKey, false);
+    Serial.println(firebaseKey + " kapatildi.");
   }
 }
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(IR_GIRIS, INPUT);
-  pinMode(IR_CIKIS, INPUT);
-  for(int i=0; i<6; i++) pinMode(IR_PARK[i], INPUT);
+  // IR sensörler için iç pull-up açıldı
+  pinMode(IR_GIRIS, INPUT_PULLUP);
+  pinMode(IR_CIKIS, INPUT_PULLUP);
 
-  // DÜZELTME: ESP32 Servo zamanlayıcıları tahsis edildi (titremeyi ve düşmeyi önler)
+  // 34 numara ESP32'de input-only pin, pullup desteklemez.
+  // Bu yüzden park sensörlerinde ayrı ayrı ayarlıyoruz.
+  pinMode(IR_PARK[0], INPUT_PULLUP); // 13
+  pinMode(IR_PARK[1], INPUT_PULLUP); // 14
+  pinMode(IR_PARK[2], INPUT_PULLUP); // 25
+  pinMode(IR_PARK[3], INPUT_PULLUP); // 26
+  pinMode(IR_PARK[4], INPUT_PULLUP); // 27
+  pinMode(IR_PARK[5], INPUT);        // 34 -> dahili pullup yok
+
+  // Servo PWM zamanlayıcıları
   ESP32PWM::allocateTimer(0);
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
@@ -100,21 +111,25 @@ void setup() {
   girisServosu.setPeriodHertz(50);
   girisServosu.attach(SERVO_GIRIS_PIN, SERVO_MIN_US, SERVO_MAX_US);
   girisServosu.write(SERVO_CLOSE_ANGLE);
-  
+
   cikisServosu.setPeriodHertz(50);
   cikisServosu.attach(SERVO_CIKIS_PIN, SERVO_MIN_US, SERVO_MAX_US);
   cikisServosu.write(SERVO_CLOSE_ANGLE);
-  
-  // DÜZELTME: detach() KOMUTLARI BURADAN DA KALDIRILDI!
-  
+
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  Serial.print("WiFi baglaniyor");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
   Serial.println("\nWiFi Baglandi!");
 
   config.api_key = API_KEY;
   config.signer.test_mode = true;
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+
+  Serial.println("Sistem hazir.");
 }
 
 void loop() {
@@ -122,56 +137,67 @@ void loop() {
 
   unsigned long currentMillis = millis();
 
-  // 1. GİRİŞ KAPISI MANTIĞI (3 Saniye Bekleme)
-  if (digitalRead(IR_GIRIS) == LOW) {
-    if (entryDetectTime == 0) {
-      entryDetectTime = currentMillis; // Gözlem başladı
-    } 
-    else if (currentMillis - entryDetectTime > 3000 && !girisOpen) { // 3 Saniye Doldu
-      if (getOccupiedCount() < 6) { 
-        openGate(girisServosu, SERVO_GIRIS_PIN, girisOpen, girisOpenTime);
-        updateFirestoreBool("entryGateOpen", true);
-        Serial.println("Arac geldi. 3 sn onaylandi. Giris bariyeri acildi.");
+  // =========================================================
+  // 1. GİRİŞ KAPISI MANTIĞI (YENİ VE KUSURSUZ)
+  // =========================================================
+  bool girisSensorAktif = (digitalRead(IR_GIRIS) == LOW);
+
+  if (girisSensorAktif) {
+    if (girisOpen) {
+      // Kapı ZATEN açıksa filtreye bakma, süreyi direkt uzat! (Titremeyi bitirir)
+      girisOpenTime = currentMillis;
+    } else {
+      // Kapı kapalıysa ilk açılış için 150ms filtreden geçmesini bekle
+      if (girisDetectStart == 0) {
+        girisDetectStart = currentMillis;
       }
-      entryDetectTime = 0; // Tekrar okumaması için sıfırla
+      if ((currentMillis - girisDetectStart >= GIRIS_DEBOUNCE_MS)) {
+        if (getOccupiedCount() < 6) {
+          openGate(girisServosu, girisOpen, girisOpenTime);
+          updateFirestoreBool("entryGateOpen", true);
+          Serial.println("Arac geldi. Giris bariyeri acildi.");
+        }
+      }
     }
   } else {
-    entryDetectTime = 0; // Araç 3 saniyeden önce giderse sayacı iptal et
+    girisDetectStart = 0;
   }
 
-  // 2. PARK ALANLARI MANTIĞI (10 sn Giriş, 5 sn Çıkış Bekleme)
-  for(int i=0; i<6; i++) {
+  // =========================================================
+  // 2. PARK ALANLARI MANTIĞI
+  // =========================================================
+  for (int i = 0; i < 6; i++) {
     bool carPresent = (digitalRead(IR_PARK[i]) == LOW);
-    
+
     if (carPresent) {
-      slotClearTime[i] = 0; // Araç var, çıkış sayacını iptal et
-      
+      slotClearTime[i] = 0;
+
       if (slotDetectTime[i] == 0) {
         slotDetectTime[i] = currentMillis;
       } 
       else if (!lastSlotStates[i] && (currentMillis - slotDetectTime[i] > 10000)) {
-        // Araç 10 SANİYE KESİNTİSİZ durdu. Firebase'e "Dolu" yaz.
         lastSlotStates[i] = true;
         updateFirestoreBool("p" + String(i) + "_occupied", true);
-        Serial.println("P" + String(i+1) + " Park Edildi (10sn Onayli).");
+        Serial.println("P" + String(i + 1) + " Park Edildi.");
       }
     } 
     else {
-      slotDetectTime[i] = 0; // Araç yok, giriş sayacını iptal et
-      
+      slotDetectTime[i] = 0;
+
       if (slotClearTime[i] == 0) {
         slotClearTime[i] = currentMillis;
       } 
       else if (lastSlotStates[i] && (currentMillis - slotClearTime[i] > 5000)) {
-        // Araç 5 SANİYE KESİNTİSİZ gitti. Firebase'e "Boş" yaz.
         lastSlotStates[i] = false;
         updateFirestoreBool("p" + String(i) + "_occupied", false);
-        Serial.println("P" + String(i+1) + " Bosaldi (5sn Onayli).");
+        Serial.println("P" + String(i + 1) + " Bosaldi.");
       }
     }
   }
 
-  // 3. ÇIKIŞ KAPISI MANTIĞI (2 Saniye Bekleme & Ödeme Kontrolü)
+  // =========================================================
+  // 3. ÇIKIŞ KAPISI MANTIĞI (GİRİŞ KAPISIYLA BİREBİR AYNI)
+  // =========================================================
   static unsigned long lastDbCheck = 0;
   if (currentMillis - lastDbCheck > 2500) {
     lastDbCheck = currentMillis;
@@ -180,23 +206,51 @@ void loop() {
     }
   }
 
-  if (digitalRead(IR_CIKIS) == LOW) {
-    if (exitDetectTime == 0) {
-      exitDetectTime = currentMillis;
-    }
-    else if (currentMillis - exitDetectTime > 2000 && !cikisOpen && carPaid) { // 2 Saniye Doldu
-      openGate(cikisServosu, SERVO_CIKIS_PIN, cikisOpen, cikisOpenTime);
-      updateFirestoreBool("exitGateOpen", true);
-      updateFirestoreBool("carPaid", false); // Diğer aracın kaçmaması için ödemeyi sıfırla
-      carPaid = false;
-      exitDetectTime = 0;
-      Serial.println("Odeme alinmis. Arac cikiyor. Bariyer acildi.");
+  bool cikisSensorAktif = (digitalRead(IR_CIKIS) == LOW);
+
+  if (cikisSensorAktif) {
+    if (cikisOpen) {
+      // Kapı ZATEN açıksa süreyi direkt uzat
+      cikisOpenTime = currentMillis;
+    } else {
+      // Kapı kapalıysa açılış için filtreyi bekle
+      if (cikisDetectStart == 0) {
+        cikisDetectStart = currentMillis;
+      }
+      if ((currentMillis - cikisDetectStart >= CIKIS_DEBOUNCE_MS)) {
+        if (carPaid) {
+          openGate(cikisServosu, cikisOpen, cikisOpenTime);
+          updateFirestoreBool("exitGateOpen", true);
+          updateFirestoreBool("carPaid", false);
+          carPaid = false;
+          Serial.println("Odeme alinmis. Arac cikiyor. Cikis bariyeri acildi.");
+        }
+      }
     }
   } else {
-    exitDetectTime = 0;
+    cikisDetectStart = 0;
   }
 
+  // =========================================================
   // 4. BARİYERLERİ OTOMATİK KAPATMA
+  // =========================================================
   closeGateIfTimeout(girisServosu, girisOpen, girisOpenTime, "entryGateOpen");
   closeGateIfTimeout(cikisServosu, cikisOpen, cikisOpenTime, "exitGateOpen");
+
+  // =========================================================
+  // 5. DEBUG
+  // =========================================================
+  static unsigned long lastDebug = 0;
+  if (currentMillis - lastDebug > 500) {
+    lastDebug = currentMillis;
+
+    Serial.print("GIRIS=");
+    Serial.print(digitalRead(IR_GIRIS));
+    Serial.print(" | CIKIS=");
+    Serial.print(digitalRead(IR_CIKIS));
+    Serial.print(" | DOLU=");
+    Serial.print(getOccupiedCount());
+    Serial.print(" | carPaid=");
+    Serial.println(carPaid ? "true" : "false");
+  }
 }
